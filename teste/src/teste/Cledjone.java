184de7fcb2dbf2254a8c4bd642f9a40b998dddeb
Cledjone <Cledjone@Cledjone-PC>
2014-08-29 19:44:37 -0300

    commit inicial do projeto

---
+++ teste/src/teste/Cledjone.java
@@ -0,0 +1,14 @@
+package teste;
+
+public class Cledjone {
+
+	public Cledjone() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
