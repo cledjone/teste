5381c4fed3155c552384e13d0cbfb4da151d4e79
Cledjone <Cledjone@Cledjone-PC>
2014-08-29 19:57:03 -0300

    comitei

---
+++ teste/src/teste/Cledjone2.java
@@ -0,0 +1,16 @@
+package teste;
+
+public class Cledjone2 {
+
+	public Cledjone2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//close
+
+	}
+
+}
